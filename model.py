910840182c97841579e42f4f0455ef6c8a0dff4e
Nicholas Seay <nicholasseay@DN0a1f27f8.SUNet>
2019-05-25 13:40:44 -0700

    added way to save weights

--- model.py
+++ model.py
@@ -9,7 +9,8 @@ from torch.utils.data import sampler
 
 import torchvision.datasets as dset
 import torchvision.transforms as T
-
+import sys
+import os
 import numpy as np
 
 NUM_TRAIN = 1000 #??????
@@ -191,26 +192,68 @@ def train(model, optimizer, epochs=1):
 
 ##### TRAINING #######
 
-learning_rate = 3e-3
+def main(argv):
+    '''
+    Pass in a model name, either to load or to save to.
+    All models will exist in the saved_models folder
+    pass in flag '-t' if you want to train a new model
+    '''
+    SAVE_PATH = './saved_models/'
+    load_model = True
+    model_name = argv[1]
+    if len(argv) > 2:
+        if argv[2] == '-t':
+            load_model = False
+    
+    learning_rate = 3e-3
 
-model = None
-optimizer = None
-################################################################################
-# Instantiate model and a corresponding optimizer #
-################################################################################
-model = SeccadeModel()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    model = None
+    optimizer = None
+    ################################################################################
+    # Instantiate model and a corresponding optimizer #
+    ################################################################################
+    model = SeccadeModel()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    
+    if not load_model:
+        # train(model, optimizer)
+        N,C,H,W = 4, 3, 64, 64
+        in1 = torch.rand((N,C,H,W))
+        in2 = torch.rand((N,C,H,W))
+        in3 = torch.rand((N,C,H,W))
+        fake_truth = torch.rand((N,C,H,W)) * 5
+        output = model(in1, in2, in3)
+        loss = torch.sum(fake_truth-output)
+        print(output.shape)
+        print ('output', output)
+        loss.backward()
+        optimizer.step()
+        print ('trained')
 
-# train(model, optimizer)
-N,C,H,W = 4, 3, 64, 64
-in1 = torch.rand((N,C,H,W))
-in2 = torch.rand((N,C,H,W))
-in3 = torch.rand((N,C,H,W))
-fake_truth = torch.rand((N,C,H,W)) * 5
-output = model(in1, in2, in3)
-loss = torch.sum(fake_truth-output)
-print(output.shape)
-print (output)
-loss.backward()
-optimizer.step()
+        if not os.path.exists(os.path.dirname(SAVE_PATH+model_name)):
+            try:
+                os.makedirs(os.path.dirname(SAVE_PATH+model_name))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+        torch.save(model.state_dict(), SAVE_PATH+model_name)
 
+    if load_model:
+        model.load_state_dict(torch.load(SAVE_PATH+model_name))
+        N,C,H,W = 4, 3, 64, 64
+        in1 = torch.ones((N,C,H,W))
+        in2 = torch.ones((N,C,H,W))
+        in3 = torch.ones((N,C,H,W))
+        output = model(in1, in2, in3)
+        print('output', output)
+
+
+if __name__ == "__main__":
+    try:
+        assert (len(sys.argv) >= 2)
+    except:
+        print('arguments:', sys.argv[1:])
+        print ('must include name of model as first argument')
+        exit()
+
+    main(sys.argv)
