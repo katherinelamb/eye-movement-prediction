updated to new 16x16 guessing output. Also found a few bugs, a big one being softmax going along wrong dimension. network IS officially learnings. With 3e3 learning rate, can get 18ish percent accuracy (random is around 6. now actual tuning should start. softmax bug not fixed for 64x64 version. while 3e3 learns, many pixels get pushed to 0 and 1 really fast. better than 3e-3 learning rate which gets 9.3 but also way too strong.

custom_modules.py
@@ -81,16 +81,59 @@ class ThreeLayerConvTransposeNet(nn.Module):
         decoding = self.softmax(x)   # (N,1,64,64)
         return decoding
 
+class TwoLayerConvNet(nn.Module):
+    '''
+    take three crop encodings and shrink to single 4x4 area to guess eye location
+    '''
+    def __init__(self, in_channel=48, channel_1=8, channel_2=1):
+        super().__init__()
+        ########################################################################
+        # Set up the layers needed for a encoding each view                    #
+        ########################################################################
+
+        self.conv1 = nn.Conv2d(in_channel, channel_1, kernel_size=3, padding=1, bias=True)
+        self.conv2 = nn.Conv2d(channel_1, channel_2, kernel_size=3, padding=1, bias=True)
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.softmax = nn.Softmax(dim=2)
+        # initialize
+        nn.init.kaiming_normal_(self.conv1.weight)
+        nn.init.constant_(self.conv1.bias, 0)
+        nn.init.kaiming_normal_(self.conv2.weight)
+        nn.init.constant_(self.conv2.bias, 0)
+
+    def forward(self, x):
+        # X starts (N,C,8,8)      (C = 48 = 3*16)
+        N = x.shape[0]
+        x = F.relu(self.conv1(x))
+        x = self.max_pool(x)        # 4x4
+        x = self.conv2(x)
+        print ('decoding pre-softmax', x)
+        # decoding = self.softmax(x) # 4x4 percentages
+        decoding = self.softmax(x.view(*x.size()[:2],-1)).view_as(x)
+        assert (decoding.shape == (N, 1, 4,4))
+        print ('decoding', decoding)
+        print ('sum', torch.sum(decoding, dim=(2,3)))
+        print ('sums of pixels over all examples', torch.sum(decoding, dim=(0,1)))
+        # exit()
+        # exit()
+        return decoding
 
 class SeccadeModel(nn.Module):
-    def __init__(self, name=None):
+    def __init__(self, name=None, version=2):
         super().__init__()
         # all SingleCropEncoders take 64x64 images
         # number after sc means original size of image before resizing
         self.sc128 = SingleCropEncoder(name)
         self.sc256 = SingleCropEncoder(name)
         self.sc512 =  SingleCropEncoder(name)
-        self.convT_net = ThreeLayerConvTransposeNet()
+        self.decoder = None
+        if version == 1:
+            convT_net = ThreeLayerConvTransposeNet()
+            self.decoder = convT_net # 64x64 outpuy
+        else:
+            conv_net = TwoLayerConvNet()
+            self.decoder = conv_net # 4x4 output
+        
         
     def forward(self, x): # x is shape (batch, channel, H, W)
         # forward encodes images as (N,16,8,8) tensors
@@ -107,8 +150,8 @@ class SeccadeModel(nn.Module):
         encodings = torch.cat((x128, x256, x512), 1) #concatenate along channel axis
         
         # decode and get distribution over pixels of guess of seccade destination
-        decoded_score = self.convT_net.forward(encodings)
-        return decoded_score
+        decoded = self.decoder.forward(encodings)
+        return decoded
 
 class PretrainModel(nn.Module):
     '''pretrains on CIFAR10'''

model.py
@@ -46,19 +46,19 @@ def main(argv):
 
     if not load_model:
         solver.train(model, optimizer)
-        N,C,H,W = 4, 3, 64, 64
-        in1 = torch.rand((N,C,H,W))
-        in2 = torch.rand((N,C,H,W))
-        in3 = torch.rand((N,C,H,W))
-        fake_truth = torch.ones((N,C,H,W)) * (1/(H*W))
-        print ('fake', fake_truth[0,0])
-        output = model(in1, in2, in3)
-        loss = torch.sum(fake_truth-output)
-        print(output.shape)
-        print ('output', output)
-        loss.backward()
-        optimizer.step()
-        print ('trained')
+        # N,C,H,W = 4, 3, 64, 64
+        # in1 = torch.rand((N,C,H,W))
+        # in2 = torch.rand((N,C,H,W))
+        # in3 = torch.rand((N,C,H,W))
+        # fake_truth = torch.ones((N,C,H,W)) * (1/(H*W))
+        # print ('fake', fake_truth[0,0])
+        # output = model(in1, in2, in3)
+        # loss = torch.sum(fake_truth-output)
+        # print(output.shape)
+        # print ('output', output)
+        # loss.backward()
+        # optimizer.step()
+        # print ('trained')
         solver.save_model(model, model_name)
 
     if load_model:

solver.py
@@ -49,7 +49,7 @@ print('using device:', device)
 SAVE_PATH = './saved_models/'
 
 
-def check_gaze_accuracy(loader, name_of_set, model):
+def check_gaze_accuracy(loader, name_of_set, model, mini):
     if loader.dataset.train:
         print('Checking accuracy on', name_of_set, 'set')
     else:
@@ -57,11 +57,12 @@ def check_gaze_accuracy(loader, name_of_set, model):
     total_percentage_points = 0
     num_samples = 0
     model.eval()  # set model to evaluation mode
+    W = 4 if mini else 64
     with torch.no_grad():
         for sample_batched in loader:
             x = sample_batched['image']
             batch_size = x.shape[0]
-            y = torch.zeros((batch_size, 64, 64))
+            y = torch.zeros((batch_size, W, W))
             coords = sample_batched['coords'].squeeze()
             # print ('coords', coords.shape, coords)
             idx = torch.arange(0, batch_size, out=torch.LongTensor())
@@ -69,9 +70,10 @@ def check_gaze_accuracy(loader, name_of_set, model):
             y = y.unsqueeze(1)
             x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
             y = y.to(device=device, dtype=dtype)
-            scores = model(x)
-            sums = torch.sum(scores, dim=(2,3), keepdim=True)
-            percentages = scores / sums
+            # scores = model(x)
+            # sums = torch.sum(scores, dim=(2,3), keepdim=True)
+            # percentages = scores / sums
+            percentages = model(x)
             # print ('percentages', percentages)
             percentages_of_correct_pixels = percentages * y
             # print ('percentages', percentages_of_correct_pixels)
@@ -118,11 +120,11 @@ def load_gaze_dataset(data_path, transform):
     # )
     # return train_loader
 
-def train(model, optimizer, epochs=1):
+def train(model, optimizer, mini=True, epochs=1):
     '''
     Train full Seccade model on our gathered data
+    mini: flag for guessing 4x4 sections of image rather than 16x16
     '''
-    CROP_SIZE = 64 #64x64 images
     NUM_TRAIN = 8000
     NUM_DEV = 9000
     DATA_TOTAL = 10000
@@ -132,20 +134,23 @@ def train(model, optimizer, epochs=1):
     # hopefully this CIFAR norm and std generalizes to our data, if not, may switch to imagenet
     transform = T.Compose([
                     gdata.ToTensor(),
-                    gdata.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                    gdata.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010), mini),
                 ])
-    dataset = gdata.GazeDataset(labels_path, data_path, train=True, transform=transform)
+    dataset = gdata.GazeDataset(labels_path, data_path, train=True, transform=transform, mini=mini)
     train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN)))#, num_workers=2)
     dev_loader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN, NUM_DEV)))#, num_workers=2)
     test_loader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=sampler.SubsetRandomSampler(range(NUM_DEV, DATA_TOTAL)))
     # for batch_idx, (data, target) in enumerate(load_dataset(data_path, transform)):
     print ('train')
+    W = 64
+    if mini:
+        W = 4
     for e in range(epochs):
         for t, sample_batched in enumerate(train_loader):
             print ('iter', t)
             x = sample_batched['image']
             batch_size = x.shape[0]
-            y = torch.zeros((batch_size, 64, 64))
+            y = torch.zeros((batch_size, W, W))
             coords = sample_batched['coords'].squeeze()
             # print ('coords', coords.shape, coords)
             idx = torch.arange(0, batch_size, out=torch.LongTensor())
@@ -181,9 +186,9 @@ def train(model, optimizer, epochs=1):
             if t % print_every == 0:
                 print('Iteration %d, loss = %.4f' % (t, loss.item()))
 #                 check_accuracy_part34(loader_train, 'train', model)
-                check_gaze_accuracy(dev_loader, 'val', model)
+                # check_gaze_accuracy(dev_loader, 'val', model, mini)
                 print()
-    check_gaze_accuracy(dev_loader, 'val', model)
+    check_gaze_accuracy(dev_loader, 'val', model, mini)
     
     
     